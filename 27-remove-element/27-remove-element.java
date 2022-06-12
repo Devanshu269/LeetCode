28fcbc8b5f31f94eeee8dd8a2e5b20639a8a5920
Devanshu shekhar <devanshu.shekhar2@gmail.com>
2022-06-12 10:00:48 +0530

    Time: 1 ms (28.04%), Space: 41.9 MB (70.04%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -2,7 +2,7 @@
 import java.util.Arrays;
 class Solution {
     public int removeElement(int[] nums, int val) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
         int insertPointer = 0;
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != val) {
