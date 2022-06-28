f8a71b0dade70532657116e44e90277ba61e2638
Devanshu shekhar <devanshu.shekhar2@gmail.com>
2022-06-28 15:45:11 +0530

    Time: 11 ms (94.38%), Space: 9.5 MB (90.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,42 +1,15 @@
 class Solution {
 public:
-    string rev(string s, int i, int j){
-        string temp;
-        while(j >= i){
-            temp += s[j];
-            j--;
-        }
-        return temp;
-    }
     string reverseWords(string s) {
-        int n = s.length();
-        if(n == 1)
-            return s;
-        int i=0, j=0;
-        string ans;
-        int flag = 0;
-        while(i<n && j<n){
-            if(s[j] != ' '){
-                j++;
-                // cout << " j for space " << j << endl;
+        for(int i = 0; i < s.size(); i++) {
+            int j = i;
+            while(s[j] != ' ' && j < s.size()) j++;
+            int k = j--;
+            while(i < j) {
+                swap(s[i], s[j]); i++; j--;
             }
-            else{
-                if(i == 0){
-                    ans += rev(s, i, j-1);
-                    j += 1, i=j;
-                }else{
-                    ans = ans + ' ' + rev(s, i, j-1);
-                    j += 1, i=j;
-                }
-                
-            }
-            if(j == n-1){
-                if(i==0)
-                    ans += rev(s, i, j);
-                else
-                    ans += ' ' + rev(s, i, j);
-            }
+            i = k;
         }
-        return ans;
+        return s;
     }
 };
